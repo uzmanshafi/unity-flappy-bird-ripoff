60d7066b0a886ecb9568d46c9bed4a8bf9a6740d
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-04-26 02:14:51 +1000

    bird and pipes added

--- flappy bird ripoff/Assets/bird_script.cs
+++ flappy bird ripoff/Assets/bird_script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class bird_script : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class bird_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ((Input.GetKeyDown(KeyCode.Space) == true))
+        {
+            myRigidbody.velocity = Vector2.up * flapForce;
+        }
         
     }
 }
