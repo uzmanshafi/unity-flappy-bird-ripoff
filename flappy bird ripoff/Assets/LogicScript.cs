e0063840cfc6bef678aae9a1895154246eb79b93
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-04-26 17:32:13 +1000

    basic flappy birds game concept is compeleted.

---
+++ flappy bird ripoff/Assets/LogicScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int PlayerScore;
+    public Text scoreText;
+
+    public GameObject gameOverPanel;
+
+[ContextMenu("Add Score")]
+    public void AddScore(int ScoreToAdd)
+    {
+        PlayerScore += ScoreToAdd;
+        scoreText.text = PlayerScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+}
--- flappy bird ripoff/Assets/bird_script.cs
+++ flappy bird ripoff/Assets/bird_script.cs
@@ -6,19 +6,27 @@ public class bird_script : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapForce;
+    public LogicScript logic;
+    public bool isDead = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.W) == true))
+        if ((Input.GetKeyDown(KeyCode.W) == true && isDead))
         {
             myRigidbody.velocity = Vector2.up * flapForce;
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            logic.GameOver();
+            isDead = false;
     }
 }
