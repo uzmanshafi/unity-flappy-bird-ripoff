f03ade7ccae06a36d2f1aad0219b41941de9d752
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-04-28 18:11:15 +1000

    flap fix

--- flappy bird ripoff/Assets/bird_script.cs
+++ flappy bird ripoff/Assets/bird_script.cs
@@ -8,6 +8,8 @@ public class bird_script : MonoBehaviour
     public float flapForce;
     public LogicScript logic;
     public bool isDead = true;
+    private bool isFlapping = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,14 @@ public class bird_script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+void Update()
+{
+    if (isDead)
     {
-        if ((Input.GetKeyDown(KeyCode.W) == true && isDead))
+        if (Input.GetKeyDown(KeyCode.W) && !isFlapping)
         {
-            myRigidbody.velocity = Vector2.up * flapForce;
+            isFlapping = true;
+            Flap();
         }
 
         if (transform.position.y > 3 || transform.position.y < -2)
@@ -27,7 +32,34 @@ public class bird_script : MonoBehaviour
             isDead = false;
             logic.GameOver();
         }
-        
+
+        // Check vertical velocity to rotate bird when falling or flapping
+        if (myRigidbody.velocity.y < 0)
+        {
+            float angle = Mathf.Lerp(0, -90, -myRigidbody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else if (isFlapping && myRigidbody.velocity.y > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 45);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
+
+
+    void Flap() {
+        myRigidbody.velocity = Vector2.up * flapForce;
+        transform.rotation = Quaternion.Euler(0, 0, 45);
+        Invoke("ResetRotation", 0.5f);
+        isFlapping = false;
+    }
+
+    void ResetRotation() {
+        transform.rotation = Quaternion.identity;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
